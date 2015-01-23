6859105f54fe7eb35b26a773e6bfee14d6bb5dd7
James McLachlan <claquer@gmail.com>
2015-01-22 21:57:58 -0500

    While

--- Chapter1/prog1.cpp
+++ Chapter1/prog1.cpp
@@ -2,13 +2,12 @@
 
 int main()
 {
-	std::cout << "Enter two numbers:" << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2
-			  << " is " << v1 + v2 << std::endl;
-	std::cout << "*/";
-	std::cout << "/*";
-	std::cout << /* "*/" /* "/*" */;
+	int sum = 0, val = 1;
+	while (val <= 10) {
+		sum += val;
+		++val;
+	}
+	std::cout << "Sum of 1 to 10 inclusive is "
+			  << sum << std::endl;
 	return 0;
 }
