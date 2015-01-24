470ca3ca6dddfa44df34cc97319ca365a3550c77
James McLachlan <claquer@gmail.com>
2015-01-23 23:42:48 -0500

    A class

--- Chapter1/prog1.cpp
+++ Chapter1/prog1.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-
+#include "Sales_item.h"
 int main()
 {
-	int sum = 0;
-	for (size_t i = -100; i < 100; ++i) {
-		sum += 1;
+	Sales_item item1, item2;
+	std::cin >> item1 >> item2;
+	if (item1.isbn() == item2.isbn()) {
+		std::cout << item1 + item2 << std::endl;
+		return 0;
+	} else {
+		std::cerr << "Data must refer to the same ISBN"
+		          << std::endl;
+		return -1;
 	}
-	std::cout << "Sum of -100 to 100 inclusive is "
-			  << sum << std::endl;
-	return 0;
 }
