b21b5d43643e2caf253d4023b014e0ea98d6f287
James McLachlan <claquer@gmail.com>
2015-01-24 22:43:59 -0500

    Reading input

--- Chapter1/prog1.cpp
+++ Chapter1/prog1.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include "Sales_item.h"
+
 int main()
 {
-	Sales_item item1, item2;
-	std::cin >> item1 >> item2;
-	if (item1.isbn() == item2.isbn()) {
-		std::cout << item1 + item2 << std::endl;
-		return 0;
+	Sales_item total;
+	if (std::cin >> total)
+	{
+		Sales_item trans;
+		while (std::cin >> trans) {
+			if (total.isbn() == trans.isbn()) {
+				total += trans;
+			}
+			else {
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
 	} else {
-		std::cerr << "Data must refer to the same ISBN"
-		          << std::endl;
+		std::cerr << "No data?!" << std::endl;
 		return -1;
 	}
+	return 0;
 }
